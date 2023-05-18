2a572a1b4d3124e0eee84549e37b29d946f6f97b
Fletcherton P Suddlesworth <flechto@gmail.com>
2023-05-18 00:58:19 -0500

    cleaned up mock

--- packages/core/src/lib/__testing__/mock-field-finder-factory.ts
+++ packages/core/src/lib/__testing__/mock-field-finder-factory.ts
@@ -3,6 +3,8 @@ import {FieldFinderFactory} from "../field-finder-factory";
 import {Lookup} from "../lookup";
 
 const mockFieldFinderFactory: FieldFinderFactory<[string, string]> = {
-    getByLabel: vi.fn(({value}: Lookup) => () => ['get-by-label', value])
+    getByLabel({value}) {
+        return () => ['get-by-label', value]
+    }
 }
 export default mockFieldFinderFactory;
