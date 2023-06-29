cafddb940ae03f840a863371eb487d45b7aa7314
Fletcherton P Suddlesworth <flechto@gmail.com>
2023-06-28 22:40:08 -0500

    added e2e for react-testing-library

---
+++ packages/react-testing-library-e2e/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-testing-library-e2e" />
+    </div>
+  );
+}
+
+export default App;
