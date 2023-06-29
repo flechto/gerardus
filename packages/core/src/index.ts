9c5214ec62589deaa9f456ddc3d8b33184222c5e
Fletcherton P Suddlesworth <flechto@gmail.com>
2023-06-28 22:48:20 -0500

    added exports to core

---
+++ packages/core/src/index.ts
@@ -0,0 +1,12 @@
+import {FieldFinderFactory} from "./lib/field-finder-factory";
+import { Lookup } from "./lib/lookup";
+import { defineMap } from "./lib/define-map";
+import { createMap } from "./lib/create-map";
+
+export {
+  FieldFinderFactory,
+  Lookup,
+
+  createMap,
+  defineMap
+}
