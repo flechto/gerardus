c63e3753affd1401cca618f46724b80b29881f30
Fletcherton P Suddlesworth <flechto@gmail.com>
2023-06-28 22:46:34 -0500

    implemented simple test page for e2e

--- packages/react-testing-library-e2e/src/app/app.tsx
+++ packages/react-testing-library-e2e/src/app/app.tsx
@@ -1,9 +1,16 @@
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
     <div>
-      <NxWelcome title="react-testing-library-e2e" />
+      <input aria-label={'Labeled By Target'} value={'Labeled'}/>
+      <div>
+        By Text Target
+      </div>
+      <button>
+        By Role Target
+      </button>
+      <div data-testid={"test-id"}>
+        Test Id Target
+      </div>
     </div>
   );
 }
